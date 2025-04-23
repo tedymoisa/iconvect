670694d5b1fa8beb019ff420ae45f628daafd3db
Tedy Gabriel Moisa <moisatedy@gmail.com>
2025-04-23 20:57:58 +0200

    fix: remove login page

--- src/app/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import { LoginForm } from "@/components/login-form";
-
-export default function LoginPage() {
-  return (
-    <div className="flex justify-center">
-      <LoginForm />
-    </div>
-  );
-}
