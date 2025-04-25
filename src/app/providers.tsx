9ac2e87fc63ffc8a6140de0e2b1478e716db0b63
Tedy Gabriel Moisa <moisatedy@gmail.com>
2025-04-25 17:53:13 +0200

    feat: add posthog analytics

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { PostHogProvider } from "@/components/posthog-provider";
 import { ThemeProvider } from "@/components/theme-provider";
 import { TRPCReactProvider } from "@/trpc/react";
 import { SessionProvider } from "next-auth/react";
@@ -8,9 +9,11 @@ import { type ReactNode } from "react";
 export default function Providers({ children }: { children: ReactNode }) {
   return (
     <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={true} disableTransitionOnChange={true}>
-      <TRPCReactProvider>
-        <SessionProvider>{children}</SessionProvider>
-      </TRPCReactProvider>
+      <PostHogProvider>
+        <TRPCReactProvider>
+          <SessionProvider>{children}</SessionProvider>
+        </TRPCReactProvider>
+      </PostHogProvider>
     </ThemeProvider>
   );
 }
--- src/env.js
+++ src/env.js
@@ -32,7 +32,9 @@ export const env = createEnv({
    */
   client: {
     NEXT_PUBLIC_API_URL: z.string(),
-    NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: z.string()
+    NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: z.string(),
+    NEXT_PUBLIC_POSTHOG_KEY: z.string(),
+    NEXT_PUBLIC_POSTHOG_HOST: z.string()
   },
 
   /**
@@ -56,6 +58,8 @@ export const env = createEnv({
     NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
     STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
     STRIPE_WEBHOOK_SECRET: process.env.STRIPE_WEBHOOK_SECRET,
+    NEXT_PUBLIC_POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY,
+    NEXT_PUBLIC_POSTHOG_HOST: process.env.NEXT_PUBLIC_POSTHOG_HOST,
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV
   },
