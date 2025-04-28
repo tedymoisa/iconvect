7af315f3e1aaf44c636974f0d41e76e52769921b
kevinagyeman <kevinagyemann@gmail.com>
2025-04-28 23:36:36 +0200

    feat: seo configuration

--- src/site-config.ts
+++ src/site-config.ts
@@ -15,7 +15,7 @@ export const siteConfig: SiteConfig = {
   description:
     "Generate stunning, unique vector icons instantly using the power of AI. Create scalable SVG assets for your projects in seconds.",
   url: "https://iconvect.com",
-  ogImage: "https://iconvect.com/og.png",
+  ogImage: "/public/seo-cover.png",
   links: {
     github: "https://github.com/project-kt/iconvect"
   },
