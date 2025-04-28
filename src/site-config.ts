c4ca948ad1f1353b57f91eb58b8e265aaf2132e9
kevinagyeman <kevinagyemann@gmail.com>
2025-04-28 23:44:31 +0200

    fix: og image

--- src/site-config.ts
+++ src/site-config.ts
@@ -15,7 +15,7 @@ export const siteConfig: SiteConfig = {
   description:
     "Generate stunning, unique vector icons instantly using the power of AI. Create scalable SVG assets for your projects in seconds.",
   url: "https://iconvect.com",
-  ogImage: "/public/seo-cover.png",
+  ogImage: "/seo-cover.png",
   links: {
     github: "https://github.com/project-kt/iconvect"
   },
