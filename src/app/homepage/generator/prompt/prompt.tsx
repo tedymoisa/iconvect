03bbc8584162a1bafa66bbaf0421311ca3d31cd6
kevinagyeman <kevinagyemann@gmail.com>
2025-05-05 00:26:33 +0200

    feat: add clear button

--- src/app/homepage/generator/prompt/prompt.tsx
+++ src/app/homepage/generator/prompt/prompt.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { scrollPage } from "@/lib/utils";
 import { useDialogStore } from "@/store/dialog";
 import { useModelStore } from "@/store/model";
 import { useSessionStore } from "@/store/session";
 import { useSvgStore } from "@/store/svg";
 import { api } from "@/trpc/react";
+import { X } from "lucide-react";
 import { useSession } from "next-auth/react";
 import type { FormEvent } from "react";
 import { useCallback, useRef, useState } from "react";
@@ -59,7 +61,15 @@ export default function Prompt() {
 
   return (
     <form ref={formRef} onSubmit={handleGenerate}>
-      <PromptTextarea prompt={prompt} setPrompt={setPrompt} formRef={formRef} />
+      <div className="flex items-start gap-4">
+        <PromptTextarea prompt={prompt} setPrompt={setPrompt} formRef={formRef} />
+        {prompt && (
+          <Button variant="secondary" size="icon" onClick={() => setPrompt("")}>
+            <X className="h-4 w-4" />
+          </Button>
+        )}
+      </div>
+
       <div className="mt-3 flex flex-col-reverse gap-2 sm:flex-row sm:items-end sm:justify-between">
         <ModelSelector isPending={isPending} session={session} />
         <GenerateButton isPending={isPending} prompt={prompt} />
